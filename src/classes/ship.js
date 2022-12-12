6a9eca2387c5b72c42b1a7934b0e5214741fed8b
Jack Watters <johnwatt@usc.edu>
2022-12-11 21:25:47 -0800

    created ship class

--- src/classes/ship.js
+++ src/classes/ship.js
@@ -2,10 +2,13 @@ export default class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
-    this.isSunk = false;
   }
 
-  hit() {}
+  hit() {
+    this.hits += 1;
+  }
 
-  isSunk() {}
+  isSunk() {
+    return this.hits === this.length;
+  }
 }
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,7 +1,41 @@
 import Ship from '../classes/ship';
 
-it('hit function that increments hit property', () => {
-  const ship = new Ship(4);
-  ship.hit();
-  expect(ship.hits).toBe(3);
+describe('ship methods', () => {
+  let testCarrier;
+  let testBattleship;
+
+  beforeEach(() => {
+    testCarrier = new Ship(5);
+    testBattleship = new Ship(4);
+  });
+
+  it('ship is hit once', () => {
+    testCarrier.hit();
+    expect(testCarrier.hits).toBe(1);
+  });
+
+  it('ship is hit multiple times', () => {
+    testBattleship.hit();
+    testBattleship.hit();
+    expect(testBattleship.hits).toBe(2);
+  });
+
+  it('check if ship is sunk (true)', () => {
+    testBattleship.hit();
+    testBattleship.hit();
+    testBattleship.hit();
+    testBattleship.hit();
+    expect(testBattleship.isSunk()).toBe(true);
+  });
+
+  it('check if ship is sunk (false)', () => {
+    testBattleship.hit();
+    testBattleship.hit();
+    testBattleship.hit();
+    expect(testBattleship.isSunk()).toBe(false);
+  });
+
+  it('ship is invalid length');
+
+  it('ship is hit more than its length');
 });

cbe0ac366e06402500fe24cb96b1feb7c379a102
Jack Watters <johnwatt@usc.edu>
2022-12-11 17:53:22 -0800

    Initial Commit

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    [new ESLintPlugin(options)],
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.html',
+      title: 'Weather App',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+};
